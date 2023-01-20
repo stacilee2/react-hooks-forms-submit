50f52a169e826160908ba0a7b55f304b6b183806
Staci Rosales <stacileep2@gmail.com>
2023-01-19 18:06:19 -0600

    commit tree 05c10fdcb22595530bc4337d8639d9a0739088b9

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,13 +11,40 @@ function Form(props) {
   function handleLastNameChange(event) {
     setLastName(event.target.value);
   }
+  const [errors, setErrors] = useState([]);
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    // first name is required
+    if (firstName.length > 0) {
+      const formData = { firstName: firstName, lastName: lastName };
+      const dataArray = [...submittedData, formData];
+      setSubmittedData(dataArray);
+      setFirstName("");
+      setLastName("");
+      setErrors([]);
+    } else {
+      setErrors(["First name is required!"]);
+    }
+  }
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      {/* conditionally render error messages */}
+      {errors.length > 0
+        ? errors.map((error, index) => (
+            <p key={index} style={{ color: "red" }}>
+              {error}
+            </p>
+          ))
+        : null}
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
   );
 }
 
